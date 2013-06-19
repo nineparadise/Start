d87e77acd78c873285c76c3a3b3a0a20b2dc7917
Polasin Intaladchum <5266263556@fitm.kmutnb.ac.th>
2013-06-19 18:07:51 +0700

    first

---
+++ www/js/index.js
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+       // var parentElement = document.getElementById(id);
+        //var listeningElement = parentElement.querySelector('.listening');
+       // var receivedElement = parentElement.querySelector('.received');
+
+       // listeningElement.setAttribute('style', 'display:none;');
+       // receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+var scanCode = function() {
+    window.plugins.barcodeScanner.scan(
+        function(result) {
+        alert("Scanned Code: " + result.text 
+                + ". Format: " + result.format
+                + ". Cancelled: " + result.cancelled);
+    }, function(error) {
+        alert("Scan failed: " + error);
+    });
+}
+
+var encodeText = function() {
+    window.plugins.barcodeScanner.encode(
+            BarcodeScanner.Encode.TEXT_TYPE,
+            "http://www.mobiledevelopersolutions.com", 
+            function(success) {
+                alert("Encode success: " + success);
+            }, function(fail) {
+                alert("Encoding failed: " + fail);
+            });
+}
+
+var encodeEmail = function() {
+    window.plugins.barcodeScanner.encode(
+        BarcodeScanner.Encode.EMAIL_TYPE,
+        "a.name@gmail.com", function(success) {
+            alert("Encode success: " + success);
+        }, function(fail) {
+            alert("Encoding failed: " + fail);
+        });
+}
+
+var encodePhone = function() {
+    window.plugins.barcodeScanner.encode(
+        BarcodeScanner.Encode.PHONE_TYPE,
+        "555-227-5283", function(success) {
+            alert("Encode success: " + success);
+        }, function(fail) {
+            alert("Encoding failed: " + fail);
+        });
+}
+
+var encodeSMS = function() {
+    window.plugins.barcodeScanner.encode(
+        BarcodeScanner.Encode.SMS_TYPE,
+        "An important message for someone.", function(success) {
+            alert("Encode success: " + success);
+        }, function(fail) {
+            alert("Encoding failed: " + fail);
+        });
+}
